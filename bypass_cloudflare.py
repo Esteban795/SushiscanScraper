fc7bf6c9f1e7108f6175a3de36e75c4cdf1e7d28
Esteban MANDJUKOWSKI <esteban.mandjukowski@telecomnancy.eu>
2023-10-12 18:17:14 +0200

    code : [+] added anime-sama support to grab URLs.

--- bypass_cloudflare.py
+++ bypass_cloudflare.py
@@ -1,12 +1,15 @@
 from seleniumbase import SB
 from bs4 import BeautifulSoup as bs
 
-
 urls = []
-with SB(uc=True) as sb: #use headless=true to not see Chrome open
+with SB(uc=True,headless=True) as sb: #use headless=true to not see Chrome open
     sb.driver.get("https://anime-sama.me/blue-lock-chapitre-217/")
-    soup = bs(sb.driver.page_source,'lxml')
-    print(sb.driver.page_source)
+    soup = bs(sb.driver.page_source,'html.parser')
+    # print(sb.driver.page_source)
     for image in soup.findAll("img"):
-        urls.append(image["src"])
-#print(urls)
+        try:
+            to_add = image["data-src"]
+        except: 
+            to_add = image["src"]
+        urls.append(to_add)
+print(urls)
